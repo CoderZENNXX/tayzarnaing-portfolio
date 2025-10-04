bf92fec7309b2eb9bd1c84db684823218b71ef99
CoderZENNXX <brocodestudent@gmail.com>
2025-10-04 23:23:39 +0700

    Project V3

--- src/JSX/App.jsx
+++ src/JSX/App.jsx
@@ -1,5 +1,7 @@
-import '../CSS/App.css'
+import "../CSS/App.css"
 import Home from "./Home.jsx"
+import Navbar from "./Navbar.jsx"
+import Footer from "./Footer.jsx"
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
 
 function App() {
@@ -7,9 +9,11 @@ function App() {
   return (
     <Router>
       <div className="app">
+        <Navbar />
         <Routes>
           <Route exact path="/" element={<Home />}/>
         </Routes>
+        <Footer />
       </div>
     </Router>
   )
---
+++ src/JSX/Navbar.jsx
@@ -0,0 +1,37 @@
+import "../CSS/App.css"
+import { Link } from "react-router-dom"
+
+function Navbar() {
+    
+    return (
+        <div className="nav">
+            <Link to="/">
+                <button className="home-btn nav-btn">
+                    Home
+                </button>
+            </Link>
+            <Link to="/about">
+                <button className="about-btn nav-btn">
+                    About
+                </button>
+            </Link>
+            <Link to="/my-story">
+                <button className="my-story-btn nav-btn">
+                    My Story
+                </button>
+            </Link>
+            <Link to="/achievements">
+                <button className="achievements-btn nav-btn">
+                    Achievements
+                </button>
+            </Link>
+            <Link to="/contact">
+                <button className="contact-btn nav-btn">
+                    Contact
+                </button>
+            </Link>
+        </div>
+    )
+}
+
+export default Navbar
