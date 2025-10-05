1fbcfe29d738b8c90c4a04fa6e7a036ba7dfba6a
CoderZENNXX <brocodestudent@gmail.com>
2025-10-05 22:25:46 +0700

    Project V6

--- src/JSX/Footer.jsx
+++ src/JSX/Footer.jsx
@@ -1,9 +1,17 @@
 import "../CSS/App.css"
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+import { faFacebook, faInstagram, faGithub, faTiktok } from "@fortawesome/free-brands-svg-icons"
 
 function Footer() {
     return (
         <div className="footer">
-            <h1>footer</h1>
+            <h1 className="footer-name">Tayzar Naing</h1>
+            <div className="footer-icon-container">
+                <a className="footer-link" href="https://github.com/CoderZENNXX" target="_blank"><FontAwesomeIcon className="footer-github-icon footer-icon" icon={faGithub} /></a>
+                <a className="footer-link" href="https://www.facebook.com/fbZENNXX" target="_blank"><FontAwesomeIcon className="footer-facebook-icon footer-icon" icon={faFacebook} /></a>
+                <a className="footer-link" href="https://www.instagram.com/insta_zennxx" target="_blank"><FontAwesomeIcon className="footer-instagram-icon footer-icon" icon={faInstagram} /></a>
+                <a className="footer-link" href="https://www.tiktok.com/@official.free.insight" target="_blank"><FontAwesomeIcon className="footer-tiktok-icon footer-icon" icon={faTiktok} /></a>
+            </div>
         </div>
     )
 }
