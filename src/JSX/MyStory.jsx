3cca470696438718c17e2c499c72123c63f07f6d
CoderZENNXX <brocodestudent@gmail.com>
2025-10-18 20:13:48 +0700

    Project V15

--- src/JSX/MyStory.jsx
+++ src/JSX/MyStory.jsx
@@ -10,14 +10,19 @@ function MyStory() {
                 </div>
                 <div className="story-paragraph-2">
                     My journey into technology started early, driven by curiosity and the urge to understand how things work. In 2024, that curiosity took form when I earned a Digital Silver Medal Certificate in the International STEM Olympiad, a recognition that showed me I was capable of more than just learning, I could create, solve, and lead. The following year, in 2025, I earned a national tutor certification from UPchieve and began tutoring students nationwide. That experience taught me that knowledge means nothing unless it’s shared.
-                    <img className="stem-olympiad-logo story-image" src="/stem-olympiad-logo.png" />
+                    <div className="story-paragraph-2-images">
+                        <img className="upchieve-logo story-image" src="/upchieve-logo.png" />
+                        <img className="stem-olympiad-logo story-image" src="/stem-olympiad-logo.png" />
+                    </div>
                 </div>
                 <div className="story-paragraph-3">
+                    <img className="free-insight-logo story-image" src="/free-insight-logo.png" />
                     That same year, I started Free Insight, an academic and educational page on TikTok where I simplify complex ideas and share insights with students from around the world. Creating content became more than just a project, it became a mission to make education engaging and accessible for everyone.
                 </div>
                 <div className="story-paragraph-4">
                     Right now, I’m focused on front-end development, crafting intuitive and interactive user experiences. But my vision doesn’t stop there, I’m working toward becoming a full-stack developer so I can build complete systems, from the backend logic to the visual experience. My dream is to develop my own technologies, tools that blend education, innovation, and accessibility, and to one day build a tech company that empowers people to learn and grow through technology.                </div>
                 <div className="story-paragraph-5">
+                    <img className="university-campus story-image" src="/classic-university-campus.jpg" />
                     Beyond coding and creating, I have a strong academic drive. I aspire to study at some of the world’s leading institutions, pushing my knowledge to its limits and surrounding myself with people who challenge and inspire me.
                 </div>
                 <div className="story-paragraph-6">
